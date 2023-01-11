535371c9a3b8818305d43e3a9f56facb0a40aa8c
yurishe0 <yurishe00@gmail.com>
2023-01-11 22:15:20 +0100

    Change the current turn to a boolean

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const player1 = playerFactory("Adam", "X");
 const player2 = playerFactory("Bob", "O");
 let player1Turn = true;
 
-
 const gameboard = (() => {
   const currentGameboard = ["", "", "", "", "", "", "", "", ""];
   /*
@@ -15,22 +14,23 @@ const gameboard = (() => {
     6 7 8
   */
 
-	const getMarker = () => {
-		if (player1Turn) {
-			return player1.marker;
-		} else {
-			return player2.marker;
-		}
-	}
+  const getMarker = () => {
+    if (player1Turn) {
+      return player1.marker;
+    } else {
+      return player2.marker;
+    }
+  };
 
   const cells = document.querySelectorAll(".cell");
   cells.forEach((cell) => {
     cell.addEventListener("click", () => {
-			if(!cell.hasChildNodes()) {
-				currentGameboard[cell.getAttribute("data-index")] = getMarker();
-				displayControl.displayGameboard();
-				switchTurn();
-			}
+      if (!cell.hasChildNodes()) {
+        currentGameboard[cell.getAttribute("data-index")] = getMarker();
+        displayControl.displayGameboard();
+        checkForWin();
+        switchTurn();
+      }
     });
   });
 
@@ -38,12 +38,37 @@ const gameboard = (() => {
     if (player1Turn) {
       player1Turn = false;
     } else {
-			player1Turn = true;
-		}
+      player1Turn = true;
     }
+  };
+
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  // 0, 3, 6, 7, 8
+
+  const checkForWin = () => {
+    for (let i = 0; i < winningCombinations.length; i++) {
+      if (
+        gameboard.currentGameboard[winningCombinations[i][0]] == getMarker() &&
+        gameboard.currentGameboard[winningCombinations[i][1]] == getMarker() &&
+        gameboard.currentGameboard[winningCombinations[i][2]] == getMarker()
+      ) {
+        console.log("Victory!");
+      }
+    }
+  };
 
   return {
     currentGameboard,
+    checkForWin,
   };
 })();
 
@@ -54,7 +79,10 @@ const displayControl = (() => {
         const cell = document.querySelector(`[data-index="${i}"]`);
 
         cell.textContent = gameboard.currentGameboard[i];
-        cell.classList.add("symbol", `symbol__${gameboard.currentGameboard[i]}`);
+        cell.classList.add(
+          "symbol",
+          `symbol__${gameboard.currentGameboard[i]}`
+        );
       }
     }
   };
