24285b179ba9a0758a5d2a882037f6d0c2e55df0
yurishe0 <yurishe00@gmail.com>
2023-01-11 21:09:45 +0100

    Listen for clicks on each cell and display a symbol when clicked using modules

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("Connected!");
+const gameboard = (() => {
+  const currentGameboard = ["", "", "", "", "", "", "", "", ""];
+  /*
+    0 1 2
+    3 4 5
+    6 7 8
+  */
+
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    cell.addEventListener("click", () => {
+      currentGameboard[cell.getAttribute("data-index")] = "X";
+      displayControl.displayGameboard();
+    });
+  });
+
+  return {
+    currentGameboard,
+  };
+})();
+
+const displayControl = (() => {
+  const displayGameboard = () => {
+    for (let i = 0; i < gameboard.currentGameboard.length; i++) {
+      if (gameboard.currentGameboard[i] != "") {
+        const cell = document.querySelector(`[data-index="${i}"]`);
+
+        cell.textContent = gameboard.currentGameboard[i];
+        cell.classList.add("symbol", "symbol__x");
+      }
+    }
+  };
+
+  return {
+    displayGameboard,
+  };
+})();
